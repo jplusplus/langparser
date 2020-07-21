64301e70cf3871871f99f970cecdfeb43886e6da
rotsee <mejl@leowallentin.se>
2020-07-21 19:35:49 +0200

    linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,40 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2,
-            {
-              "FunctionDeclaration": {
-                "parameters": "first"
-              },
-              "VariableDeclarator": {
-                "var": 2, "let": 2, "const": 3
-              },
-              "ignoredNodes": [
-                "VariableDeclarator[init.type='CallExpression'][init.callee.name='require'] > ObjectPattern"
-              ],
-              "SwitchCase": 1,
-            },
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2,
+      {
+        "FunctionDeclaration": {
+          "parameters": "first"
+        },
+        "VariableDeclarator": {
+          "var": 2, "let": 2, "const": 3
+        },
+        "ignoredNodes": [
+          "VariableDeclarator[init.type='CallExpression'][init.callee.name='require'] > ObjectPattern"
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
-};
+        "SwitchCase": 1,
+      },
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ]
+  }
+}
